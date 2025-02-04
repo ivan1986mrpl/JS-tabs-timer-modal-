89e0b9809397bfdfdb1a9c23d46cbf57d441a3f6
ivan1986mrpl <121858281+ivan1986mrpl@users.noreply.github.com>
2025-02-04 14:46:09 +0200

    add modal

--- js/script.js
+++ js/script.js
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', () => {
     //==================== //TABS ==========================
 
     //==================== Timer ===========================
-    const deadline = '2025-02-04';
+    const deadline = '2025-03-04';
 
     function getTimeRemaining(endTime) {// разница милисекунд между датами и расчет показателей
         let days, hours, minutes, seconds;
@@ -104,6 +104,55 @@ document.addEventListener('DOMContentLoaded', () => {
 
     setClock('.timer', deadline);
     //==================== //Timer =========================
+
+    //====================== Modal =========================
+    const modalTriggers = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('[data-close]');
+
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';//запрещаем скрол страницы под модальным окном
+        clearInterval(modalTimerId);//если пользователь уже открывал окно, оно не будет открываться по истечению интервала (убрать строку, если не используется открытие по таймеру)
+    }
+
+    function closeModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+
+    modalTriggers.forEach(btnOpen => {
+        btnOpen.addEventListener('click', openModal);
+    });
+
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {//при клике на подложку модальное окно закроется
+        if (e.target === modal) {
+            closeModal();
+        }        
+    });
+
+    document.addEventListener('keydown', (e) => {//закрытие при клике на Escape
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+    
+    const modalTimerId = setTimeout(openModal, 4000); //открытие окна после 4 секунд
+
+    //открытие модального окна при пролистывании страницы вниз до конца, сработает обработчик только один раз
+    function showModalByScroll() {
+        if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);//удаляем обработчик события, если уже 1 раз при скролле до конца страницы окно открывалось
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
+    //==================== //Modal =========================
 
 
 
